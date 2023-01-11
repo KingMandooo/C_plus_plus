da8fe22643f02fb3e04a1978f907201d3eb64737
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2023-01-11 15:41:22 +0900

    객체지향의 시발점

--- 윤성우 열혈 C++ 프로그래밍/Chapter 04/04-5 : OOP 단계별 프로젝트 02단계.cpp
+++ 윤성우 열혈 C++ 프로그래밍/Chapter 04/04-5 : OOP 단계별 프로젝트 02단계.cpp
@@ -178,3 +178,172 @@ void ShowAllAccInfo()
 	}
 }
 
+
+----------------------------------------------------------------------------------------------------
+
+💙 성우형 코드 💙
+	
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void ShowMenu();       // 메뉴츌력
+void MakeAccount();    // 계좌개설을 위한 함수
+void DepositMoney();   // 입     금
+void WidthdrawMoney(); // 출     금
+void ShowAllAccInfo(); // 잔액조회
+
+enum { Make = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+class Account
+{
+private:
+	int accID;      // 아이디
+	int balance;    // 잔액
+	char* cusName;  // 고객 이름
+
+public:
+	Account(int id, int money, char* name) : accID(id), balance(money)
+	{
+		int len = strlen(name) + 1;
+		cusName = new char[len];
+		strcpy(cusName, name);
+	}
+
+	int GetAccID() { return accID; }   // 🎈🎈 나는 이생각을 못함 : private 멤버변수를 클래스 안에서만 해결하려고 함.
+
+	void Deposit(int money)
+	{
+		balance += money;
+	}
+
+	int Widthdraw(int money)
+	{
+		if (balance < money)
+			return 0;
+		
+		balance -= money;
+		return money;
+	}
+
+	void ShowAccInfo()
+	{
+		cout << "계좌ID: " << accID << endl;
+		cout << "이 름: " << cusName << endl;
+		cout << "입금액: " << balance << endl << endl;
+	}
+
+	~Account()
+	{
+		delete[] cusName;
+	}
+};
+
+Account* accArr[100];
+int accNum = 0;
+
+int main()
+{
+	int choice;
+
+	while (true)
+	{
+		ShowMenu();
+		cout << "선택: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case Make:MakeAccount(); break;
+		case DEPOSIT:DepositMoney(); break;
+		case WITHDRAW:WidthdrawMoney(); break;
+		case INQUIRE:ShowAllAccInfo(); break;
+		case EXIT: return 0;
+		default: cout << "Illegal selection.." << endl; break;
+		}
+	}
+
+	return 0;
+}
+
+void ShowMenu()
+{
+	cout << "-----Menu-----" << '\n';
+	cout << "1. 계좌개설\n";
+	cout << "2. 입 금\n";
+	cout << "3. 출 금\n";
+	cout << "4. 계좌정보 전체 출력\n";
+	cout << "5. 프로그램 종료\n\n";
+}
+
+void MakeAccount()
+{
+	int id, money;
+	char name[50];
+
+	cout << "[계좌개설]\n";
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "이 름: ";
+	cin >> name;
+	cout << "입금액: ";
+	cin >> money;
+
+	accArr[accNum] = new Account(id, money, name);  // 동적할당
+	accNum++;
+	cout << endl;
+}
+
+void DepositMoney()
+{
+	int money, id;
+
+	cout << "[입  금]\n";
+	cout << "계좌ID: "; cin >> id;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccID() == id)
+		{
+			cout << "입금액: "; cin >> money;
+			accArr[i]->Deposit(money);
+			cout << "입금완료" << endl << endl;
+			return;
+		}
+	}
+
+	cout << "유효하지 않는 ID 입니다.\n\n";    // 나이스한 방법?!
+}
+
+void WidthdrawMoney()
+{
+	int id, money;
+
+	cout << "[출  금]\n";
+	cout << "계좌ID: "; cin >> id;
+	cout << "출금액: "; cin >> money;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccID() == id)
+		{
+			if(accArr[i]->Widthdraw(money) == 0)
+				cout << "잔액부족\n";
+			else
+				cout << "출금완료\n\n";
+
+			return;
+		}
+	}
+
+	cout << "유효하지 않는 ID 입니다.\n\n";
+}
+
+void ShowAllAccInfo()
+{
+	for (int i = 0; i < accNum; i++)
+	{
+		accArr[i]->ShowAccInfo();
+	}
+}
+
