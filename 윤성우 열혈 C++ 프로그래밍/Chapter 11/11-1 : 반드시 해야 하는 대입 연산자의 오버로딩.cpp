afcef993483d7956ac8db0aaaf10495a01924878
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2023-01-25 21:58:31 +0900

    Update 11-1 : 반드시 해야 하는 대입 연산자의 오버로딩.cpp

--- 윤성우 열혈 C++ 프로그래밍/Chapter 11/11-1 : 반드시 해야 하는 대입 연산자의 오버로딩.cpp
+++ 윤성우 열혈 C++ 프로그래밍/Chapter 11/11-1 : 반드시 해야 하는 대입 연산자의 오버로딩.cpp
@@ -133,3 +133,370 @@ int main()
 -----------------------------------------------------------------------------------
   
 🟢 상속 구조에서의 대입 연산자 호출
+
+  --> 🎈🎈 유도 클래스의 대입 연산자에는 아무런 명시를 하지 않으면, 기초 클래스의 대입 연산자가 호출되지 않는다.
+
+	
+#include <iostream>
+using namespace std;
+
+class First
+{
+private:
+	int num1, num2;
+public:
+	First(int n1 = 0, int n2 = 0) : num1(n1), num2(n2)
+	{}
+	void ShowData() { cout << num1 << ", " << num2 << endl; }
+
+	First& operator=(const First& ref)
+	{
+		cout << "First& operator=()" << endl;
+		num1 = ref.num1;
+		num2 = ref.num2;
+		return *this;
+	}
+};
+
+class Second : public First
+{
+private:
+	int num3, num4;
+public:
+	Second(int n1, int n2, int n3, int n4) : First(n1, n2), num3(n3), num4(n4)
+	{}
+	void ShowData()
+	{
+		First::ShowData();
+		cout << num3 << ", " << num4 << endl;
+	}
+};
+
+int main()
+{
+	Second ssrc(111, 222, 333, 444);
+	Second scpy(0, 0, 0, 0);
+	scpy = ssrc;                  // scpy.operator=(ssrc); <-- 여기서 Second 클래스의 디폴트 대입 연산자가 호출된다.
+	scpy.ShowData();              //                           그리고 그 디폴트 대입 연산자가 기초 클래스의 대입 연산자까지 호출해준다.
+
+	return 0;
+}
+
+                                          🎈🎈 유도 클래스의 디폴트 대입 연산자는 기초 클래스의 대입 연산자까지 호출한다는 사실을 알 수 있다.
+/*                                             이거 잘 이해해야 한다.
+  출력결과:     First& operator=()  
+		111, 222
+		333, 444
+*/
+------------------------------------------------
+						  
+#include <iostream>
+using namespace std;
+
+class First
+{
+private:
+	int num1, num2;
+public:
+	First(int n1 = 0, int n2 = 0) : num1(n1), num2(n2)
+	{}
+	void ShowData() { cout << num1 << ", " << num2 << endl; }
+
+	First& operator=(const First& ref)
+	{
+		cout << "First& operator=()" << endl;
+		num1 = ref.num1;
+		num2 = ref.num2;
+		return *this;
+	}
+};
+
+class Second : public First
+{
+private:
+	int num3, num4;
+public:
+	Second(int n1, int n2, int n3, int n4) : First(n1, n2), num3(n3), num4(n4)
+	{}
+	void ShowData()
+	{
+		First::ShowData();
+		cout << num3 << ", " << num4 << endl;
+	}
+
+	Second& operator=(const Second& ref)                // 🎈🎈 즉, 이렇게 유도 클래스의 대입 연산자를 정의해야 하는 경우에는 
+	{                                                   //       기초 클래스의 대입 연산자를 명시적으로 호출해야 한다.
+	
+		First::operator=(ref);  // 👉👉기초 클래스 대입 연산자 명시적으로 호출.  만약 이게 없으면,
+		cout << "Second& operator=()" << endl;                                // 당연히 Second 객체의 num1과 num2는 초기화 되지 않는다.
+		num3 = ref.num3;
+		num4 = ref.num4;
+		return *this;
+	}
+};
+
+int main()
+{
+	Second ssrc(111, 222, 333, 444);
+	Second scpy(0, 0, 0, 0);
+	scpy = ssrc;
+	scpy.ShowData();
+
+	return 0;
+}
+
+/*                                           
+  출력결과:     First& operator=()  
+		111, 222
+		333, 444
+*/
+
+------------------------------------------------------------------------------------------------
+	
+🟢 이니셜라이저가 성능 향상에 도움을 준다고 했던 것을 기억하나요?
+	
+#include <iostream>
+using namespace std;
+
+class AAA
+{
+private:
+	int num; 
+public:
+	AAA(int n = 0) : num(n)      // 🎈🎈 int num = n   -->   AAA의 생성자 호출
+	{
+		cout << "AAA(int n = 0)" << endl;
+	}
+	AAA(const AAA& ref) : num(ref.num)
+	{
+		cout << "AAA(const AAA& ref)" << endl;
+	}
+	AAA& operator=(const AAA& ref)
+	{
+		num = ref.num;
+		cout << "operator=(const AAA& ref)" << endl;
+		return *this;
+	}
+};
+
+class BBB
+{
+private:
+	AAA mem;
+public:
+	BBB(const AAA& ref) : mem(ref) {}   // 🎈🎈 AAA mem = ref  -->  AAA의 복사 생성자 호출
+};
+
+class CCC
+{
+private:
+	AAA mem;                             // AAA는 이미 생성 및 초기화가 진행된 객체이므로 여기서의 '='은 대입 연산자를 호출한다.
+public:
+	CCC(const AAA& ref) { mem = ref; }   // 🎈🎈 AAA mem;      -->  AAA의 생성자 호출
+};                                           //      mem = ref;    -->  AAA의 대입연산자 호출
+
+int main()
+{
+	AAA obj1(12);
+	cout << "********************" << endl;
+	BBB obj2(obj1);
+	cout << "********************" << endl;
+	CCC obj3(obj1);
+
+	return 0;        
+}
+                                          ✅ 이렇듯 위 코드에서 보았듯이 이니셜라이저를 이용해서 초기화를 진행하면,
+                                              함수의 호출횟수를 줄일 수 있어서, 그리고 초기화의 과정을 단순화시킬 수 있어서
+					      약간의 성능향상을 기대할 수 있다.
+/*                                           
+  출력결과:     AAA(int n = 0)
+		********************
+		AAA(const AAA& ref)
+		********************
+		AAA(int n = 0)
+		operator=(const AAA& ref)
+*/
+
+------------------------------------------------------------------------------------------------
+🟠 문제 11-1(1)
+
+💙 내가 푼 방식 💙
+
+#include <iostream>
+#include <cstring>                          
+using namespace std;
+
+class Gun
+{
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {}
+	int GetBullet() { return bullet; }  // 총알 가져오는 함수 추가
+	void Shot()
+	{
+		cout << "BBang!" << endl;
+		bullet--;
+	}
+};
+
+class Police      
+{
+private:
+	int handcuffs;
+	Gun* pistol;
+public:
+	Police(int bnum, int bcuff) : handcuffs(bcuff)
+	{
+		if (bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
+	}
+	Police(Police& copy)  // 복사 생성자
+	{
+		handcuffs = copy.handcuffs;
+		pistol = new Gun(copy.pistol->GetBullet());
+	}
+	Police& operator=(Police& ref) // 대입 연산자
+	{
+		delete pistol;
+		handcuffs = ref.handcuffs;
+		pistol = new Gun(ref.pistol->GetBullet());
+		return *this;
+	}
+	void PutHandcuff()
+	{
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+	void Shot()
+	{
+		if (pistol == NULL)
+			cout << "No Gun!!" << endl;
+		else
+			pistol->Shot();
+	}
+	~Police()
+	{
+		if (pistol != NULL)
+			delete pistol;
+	}
+};
+
+int main()
+{
+	Police pman1(5, 3);
+
+	//Police pman2 = pman1;
+
+	Police pman2(1, 2);
+	pman2 = pman1;
+
+	pman2.Shot();
+	pman2.PutHandcuff();
+
+	return 0;
+}
+
+----------------------------------------------------------
+💙 답지 💙
+
+#include <iostream>
+#include <cstring>                          
+using namespace std;
+
+class Gun
+{
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {}
+	Gun(Gun& copy) : bullet(copy.bullet)  // 내가 추가한 복사생성자 (호출되는지 확인용)
+	{
+		cout << "복사생성자    ㅇㅇ " << endl;
+	}
+	void Shot()
+	{
+		cout << "BBang!" << endl;
+		bullet--;
+	}
+};
+
+class Police
+{
+private:
+	int handcuffs;
+	Gun* pistol;
+public:
+	Police(int bnum, int bcuff) : handcuffs(bcuff)
+	{
+		if (bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
+	}
+	Police(Police& copy)  // 복사 생성자
+	{
+		handcuffs = copy.handcuffs;
+		pistol = new Gun(*(copy.pistol)); // Gun의 복사생성자 호출
+	}
+
+	// pistol = new Gun(*(copy.pistol)); 이게 무슨 뜻일까? 
+	// 그니까 내가 궁금한건 왜 * 를 붙여주는 걸까?
+	// copy.pistol은 pos1.pistol이고 pistol은 주소 값일테고,
+	// 그렇다면 *(copy.pistol), *(pos1.pistol)은 뭐지?
+	// copy.pistol(객체의 주소)이 가리키는 객체이겠다.
+	// pistol에는 주소값이 저장 돼. Gun의 객체의 주소값이 저장 돼.
+	// 이를 가리키는 표현은 *(copy.pistol) 이거야.
+	// 그리고 Gun의 복사생성자에서 Gun& copy는 참조자이므로 값을 받아야 돼.
+	// 따라서 포인터가 붙어야 돼.
+	// int num = 3;
+	// int& num2 = num;  이렇게 값을 받잖아. 참조자는.
+	// 그렇다면 이제 이해할 수 있어.
+	// pistol = new Gun(*(copy.pistol)); 은 pistol에 새로운 매모리를 참조하도록 함과 동시에
+	// Gun 클래스의 복사생성자를 호출하는 거야. 그러면 Gun의 객체인 pistol의 멤버변수 bullet이
+	// 복사가 되겠지.
+	
+	Police& operator=(Police& ref) // 대입 연산자
+	{
+		delete pistol;
+		handcuffs = ref.handcuffs;
+		pistol = new Gun(*(ref.pistol));  // Gun의 복사생성자 호출
+		return *this;
+	}
+	void PutHandcuff()
+	{
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+	void Shot()
+	{
+		if (pistol == NULL)
+			cout << "No Gun!!" << endl;
+		else
+			pistol->Shot();
+	}
+	~Police()
+	{
+		if (pistol != NULL)
+			delete pistol;
+	}
+};
+
+int main()
+{
+	Police pman1(5, 3);
+	Police pman2 = pman1;
+	Police pman3(4, 7);
+	pman3 = pman1;
+
+	pman2.Shot();
+	pman2.PutHandcuff();
+
+	return 0;
+}
+
+
+
+🟠 문제 11-1(2)
