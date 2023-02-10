ed0f238288827c85499d009e661da710b74d578f
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2023-02-10 15:01:52 +0900

    직접 구현해보는 string 클래스

--- 윤성우 열혈 C++ 프로그래밍/Chapter 12/12-2 : 문자열 처리 클래스의 정의.cpp
+++ 윤성우 열혈 C++ 프로그래밍/Chapter 12/12-2 : 문자열 처리 클래스의 정의.cpp
@@ -1,3 +1,7 @@
+직접 구현해보는 string 클래스
+
+💙 내가 짠 코드 💙
+
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -28,17 +32,17 @@ public:
 	}
 	String operator+(const String& copy)
 	{
-		char* s = new char[strlen(arr) + strlen(copy.arr) - 1];
+		char* s = new char[strlen(arr) + strlen(copy.arr) + 1];
 		strcpy(s, arr);
 		strcat(s, copy.arr);
 
 		String ss = s;
-		delete[] s;    // 왜 여기서 오류 발생.?
+		delete[] s;    
 		return ss;
 	}
 	String& operator+=(const String& copy)
 	{
-		char* s = new char[strlen(arr) + strlen(copy.arr) - 1];
+		char* s = new char[strlen(arr) + strlen(copy.arr) + 1];
 		strcpy(s, arr);
 		strcat(s, copy.arr);
 
@@ -101,111 +105,116 @@ int main()
 	return 0; 
 }
 
+------------------------------------------------------------------
 
-//#include <iostream>
-//#include <cstring>
-//using namespace std;
-//
-//class String
-//{
-//private:
-//	int len;
-//	char* str;
-//public:
-//	String()
-//	{
-//		len = 0;
-//		str = NULL;
-//	}
-//	String(const char* s)
-//	{
-//		len = strlen(s) + 1;
-//		str = new char[len];
-//		strcpy(str, s);
-//	}
-//	String(const String& s)
-//	{
-//		len = s.len;
-//		str = new char[len];
-//		strcpy(str, s.str);
-//	}
-//	~String()
-//	{
-//		if (str != NULL)
-//			delete[]str;
-//	}
-//	String& operator=(const String& s)
-//	{
-//		if (str != NULL)
-//			delete[] str;
-//		len = s.len;
-//		str = new char[len];
-//		strcpy(str, s.str);
-//		return *this;
-//	}
-//	String& operator+=(const String& s)
-//	{
-//		len += (s.len - 1);
-//		char* tempstr = new char[len];
-//		strcpy(tempstr, str);
-//		strcat(tempstr, s.str);
-//
-//		if (str != NULL)
-//			delete[] str;
-//		str = tempstr;
-//		return *this;
-//	}
-//	bool operator==(const String& s)
-//	{
-//		return strcmp(str, s.str) ? false : true;
-//	}
-//	String operator+ (const String& s)
-//	{
-//		char* tempstr = new char[len + s.len - 1];
-//		strcpy(tempstr, str);
-//		strcat(tempstr, s.str);
-//
-//		String temp(tempstr);
-//		delete[] tempstr;
-//		return temp;
-//	}
-//	friend ostream& operator<<(ostream& os, const String& copy);
-//	friend istream& operator>>(istream& is, String& copy);
-//};
-//
-//ostream& operator<<(ostream& os, const String& copy)
-//{
-//	os << copy.str;
-//	return os;
-//}
-//
-//istream& operator>>(istream& is, String& copy)
-//{
-//	char str[100];
-//	is >> str;
-//	copy = String(str);
-//	return is;
-//}
-//
-//int main() 
-//{
-//	String str1 = "I like ";
-//	String str2 = "string class";
-//	String str3 = str1 + str2;
-//
-//	cout << str1 << endl;
-//	cout << str2 << endl;
-//	cout << str3 << endl;
-//
-//	str1 += str2;
-//	if (str1 == str3)
-//		cout << "동일 문자열!" << endl;
-//	else
-//		cout << "동일하지 않은 문자열!" << endl;
-//
-//	String str4;
-//	cout << "문자열 입력: ";
-//	cin >> str4;
-//	cout << "입력한 문자열: " << str4 << endl;
-//	return 0; 
-//}
+💙 책 코드 💙
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class String
+{
+private:
+	int len;
+	char* str;
+public:
+	String()
+	{
+		len = 0;
+		str = NULL;
+	}
+	String(const char* s)
+	{
+		len = strlen(s) + 1;
+		str = new char[len];
+		strcpy(str, s);
+	}
+	String(const String& s)
+	{
+		len = s.len;
+		str = new char[len];
+		strcpy(str, s.str);
+	}
+	~String()
+	{
+		if (str != NULL)
+			delete[]str;
+	}
+	String& operator=(const String& s)
+	{
+		if (str != NULL)
+			delete[] str;
+		len = s.len;
+		str = new char[len];
+		strcpy(str, s.str);
+		return *this;
+	}
+	String& operator+=(const String& s)
+	{
+		len += (s.len - 1);
+		char* tempstr = new char[len];
+		strcpy(tempstr, str);
+		strcat(tempstr, s.str);
+
+		if (str != NULL)
+			delete[] str;
+		str = tempstr;
+		return *this;
+	}
+	bool operator==(const String& s)
+	{
+		return strcmp(str, s.str) ? false : true;
+	}
+	String operator+ (const String& s)
+	{
+		char* tempstr = new char[len + s.len - 1];
+		strcpy(tempstr, str);
+		strcat(tempstr, s.str);
+
+		String temp(tempstr);
+		delete[] tempstr;
+		return temp;
+	}
+	friend ostream& operator<<(ostream& os, const String& copy);
+	friend istream& operator>>(istream& is, String& copy);
+};
+
+ostream& operator<<(ostream& os, const String& copy)
+{
+	os << copy.str;
+	return os;
+}
+
+istream& operator>>(istream& is, String& copy)
+{
+	char str[100];
+	is >> str;
+	copy = String(str);
+	return is;
+}
+
+int main()
+{
+	String str1 = "I like ";
+	String str2 = "string class";
+	String str3 = str1 + str2;
+
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+
+	str1 += str2;
+	if (str1 == str3)
+		cout << "동일 문자열!" << endl;
+	else
+		cout << "동일하지 않은 문자열!" << endl;
+
+	String str4;
+	cout << "문자열 입력: ";
+	cin >> str4;
+	cout << "입력한 문자열: " << str4 << endl;
+	return 0;
+}
+
+
